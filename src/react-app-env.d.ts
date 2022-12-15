c0458399412e20c12b6e176d749ccf51faaaf737
andre10w <andrewietusch10@gmail.com>
2022-12-15 01:05:51 +0000

    declare js modules

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,6 @@
+// <reference types="react-scripts" />
+declare module "three";
+declare module "three/examples/jsm/controls/OrbitControls.js";
+declare module "three/examples/jsm/loaders/GLTFLoader.js";
+declare module "three/examples/jsm/loaders/HDRCubeTextureLoader.js";
+declare module "three/examples/jsm/loaders/DRACOLoader.js";
